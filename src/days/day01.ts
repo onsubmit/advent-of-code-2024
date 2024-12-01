70634022eb82a69a1f4a9b9e3dcaf93d66995b52
Andy <onsubmit@outlook.com>
2024-11-30 21:07:11 -0800

    Add day 1 part 2

--- src/days/day01.ts
+++ src/days/day01.ts
@@ -20,6 +20,26 @@ export const getPartOneSolution = (input: string): string => {
   return sum.toString();
 };
 
-export const getPartTwoSolution = (_input: string): string => {
-  return '';
+export const getPartTwoSolution = (input: string): string => {
+  const lines = input.split('\n').filter(Boolean);
+
+  let left: Array<number> = [];
+  let right: Array<number> = [];
+  for (const line of lines) {
+    const split = line.split(' ').filter(Boolean);
+    left.push(parseInt(split[0], 10));
+    right.push(parseInt(split[1], 10));
+  }
+
+  left = left.sort();
+  right = right.sort();
+
+  let sum = 0;
+  for (let i = 0; i < left.length; i++) {
+    const l = left[i];
+    const found = right.filter((r) => r === l).length;
+    sum += l * found;
+  }
+
+  return sum.toString();
 };
