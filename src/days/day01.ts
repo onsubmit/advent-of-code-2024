7ebafd1104168378352cf1f5c66399fd223f1426
Andy <onsubmit@outlook.com>
2024-11-30 21:18:13 -0800

    Clean up day 1

--- src/days/day01.ts
+++ src/days/day01.ts
@@ -1,45 +1,27 @@
+import { sumArray } from '../arrayMethods';
+
 export const getPartOneSolution = (input: string): string => {
-  const lines = input.split('\n').filter(Boolean);
-
-  let left: Array<number> = [];
-  let right: Array<number> = [];
-  for (const line of lines) {
-    const split = line.split(' ').filter(Boolean);
-    left.push(parseInt(split[0], 10));
-    right.push(parseInt(split[1], 10));
-  }
-
-  left = left.sort();
-  right = right.sort();
-
-  let sum = 0;
-  for (let i = 0; i < left.length; i++) {
-    sum += Math.abs(left[i] - right[i]);
-  }
-
-  return sum.toString();
+  const { left, right } = parseInput(input);
+  const distances = left.map((v, i) => Math.abs(v - right[i]));
+  return sumArray(distances).toString();
 };
 
 export const getPartTwoSolution = (input: string): string => {
+  const { left, right } = parseInput(input);
+  const scores = left.map((v) => v * right.filter((r) => r === v).length);
+  return sumArray(scores).toString();
+};
+
+const parseInput = (input: string): { left: Array<number>; right: Array<number> } => {
   const lines = input.split('\n').filter(Boolean);
 
-  let left: Array<number> = [];
-  let right: Array<number> = [];
+  const left: Array<number> = [];
+  const right: Array<number> = [];
   for (const line of lines) {
     const split = line.split(' ').filter(Boolean);
     left.push(parseInt(split[0], 10));
     right.push(parseInt(split[1], 10));
   }
 
-  left = left.sort();
-  right = right.sort();
-
-  let sum = 0;
-  for (let i = 0; i < left.length; i++) {
-    const l = left[i];
-    const found = right.filter((r) => r === l).length;
-    sum += l * found;
-  }
-
-  return sum.toString();
+  return { left: left.sort(), right: right.sort() };
 };
