cbb01e2403099985fc949622ddb9e65d9536dd7a
Andy <onsubmit@outlook.com>
2024-12-01 22:00:48 -0800

    Add countWhere

--- src/arrayMethods.ts
+++ src/arrayMethods.ts
@@ -3,3 +3,14 @@ export const sumArray = (arr: number[]) => arr.reduce((acc, a) => acc + a, 0);
 export const multiplyArray = (arr: number[]) => arr.reduce((acc, a) => acc * a, 1);
 
 export const sortArray = (arr: number[]) => arr.sort((a, b) => a - b);
+
+export const countWhere = <T>(arr: T[], fn: (v: T) => boolean): number => {
+  let count = 0;
+  for (const row of arr) {
+    if (fn(row)) {
+      count++;
+    }
+  }
+
+  return count;
+};
--- src/inputHelper.ts
+++ src/inputHelper.ts
@@ -11,11 +11,14 @@ const splitLines = memoizee(
 
 export const inputTo2dArray = <T, TValue extends string = string>(
   input: string,
+  columnSplitter: string,
   mapper: (character: TValue, row: number, column: number) => T,
   options: { retainEmptyLines: boolean } = { retainEmptyLines: false }
 ): Array<Array<T>> => {
   const lines = splitLines(input, options);
   return lines.map<T[]>((line, row) =>
-    [...line].map<T>((character, column) => mapper(character as TValue, row, column))
+    line
+      .split(columnSplitter)
+      .map<T>((character, column) => mapper(character as TValue, row, column))
   );
 };
