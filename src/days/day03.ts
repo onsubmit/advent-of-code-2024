0dbdf2dc8e53dca24853792ed64b53a37386b67b
Andy <onsubmit@outlook.com>
2024-12-02 22:18:58 -0800

    Clean up day 3

--- src/days/day03.ts
+++ src/days/day03.ts
@@ -1,14 +1,10 @@
+import { sumArrayBy } from '../arrayMethods';
+
 export const getPartOneSolution = (input: string): string => {
   const lines = input.split('\n').filter(Boolean);
   const line = lines.join('');
 
-  const matches = line.matchAll(/mul\((\d+),(\d+)\)/g);
-  let sum = 0;
-  for (const match of matches) {
-    const a = parseInt(match[1], 10);
-    const b = parseInt(match[2], 10);
-    sum += a * b;
-  }
+  const sum = getSumOfProducts(line);
   return sum.toString();
 };
 
@@ -20,7 +16,7 @@ export const getPartTwoSolution = (input: string): string => {
   let index1 = 0;
   let index2 = 0;
   let count = 0;
-  let delimiters = ["don't()", 'do()'] as const;
+  const delimiters = ["don't()", 'do()'] as const;
   let delim: string = '';
   while (((delim = delimiters[count++ % 2]), (index2 = line.indexOf(delim, index1)) > -1)) {
     split.push(line.substring(index1, index2));
@@ -29,17 +25,18 @@ export const getPartTwoSolution = (input: string): string => {
 
   split.push(line.substring(index1));
 
+  const sum = sumArrayBy(split, (line, i) => (i % 2 ? 0 : getSumOfProducts(line)));
+  return sum.toString();
+};
+
+const getSumOfProducts = (line: string): number => {
   let sum = 0;
-  for (let i = 0; i < split.length; i++) {
-    if (i % 2 === 1) continue;
-    const line = split[i];
-    const matches = line.matchAll(/mul\((\d+),(\d+)\)/g);
-    for (const match of matches) {
-      const a = parseInt(match[1], 10);
-      const b = parseInt(match[2], 10);
-      sum += a * b;
-    }
+  const matches = line.matchAll(/mul\((\d+),(\d+)\)/g);
+  for (const match of matches) {
+    const a = parseInt(match[1], 10);
+    const b = parseInt(match[2], 10);
+    sum += a * b;
   }
 
-  return sum.toString();
+  return sum;
 };
