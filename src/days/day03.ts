111b639881cfabbbc2df33e556a61af2976fe5f7
Andy <onsubmit@outlook.com>
2024-12-02 22:02:12 -0800

    Add day 3, part 2

--- src/days/day03.ts
+++ src/days/day03.ts
@@ -13,5 +13,33 @@ export const getPartOneSolution = (input: string): string => {
 };
 
 export const getPartTwoSolution = (input: string): string => {
-  return '';
+  const lines = input.split('\n').filter(Boolean);
+  const line = lines.join('');
+
+  const split: Array<string> = [];
+  let index1 = 0;
+  let index2 = 0;
+  let count = 0;
+  let delimiters = ["don't()", 'do()'] as const;
+  let delim: string = '';
+  while (((delim = delimiters[count++ % 2]), (index2 = line.indexOf(delim, index1)) > -1)) {
+    split.push(line.substring(index1, index2));
+    index1 = index2 + delim.length;
+  }
+
+  split.push(line.substring(index1));
+
+  let sum = 0;
+  for (let i = 0; i < split.length; i++) {
+    if (i % 2 === 1) continue;
+    const line = split[i];
+    const matches = line.matchAll(/mul\((\d+),(\d+)\)/g);
+    for (const match of matches) {
+      const a = parseInt(match[1], 10);
+      const b = parseInt(match[2], 10);
+      sum += a * b;
+    }
+  }
+
+  return sum.toString();
 };
