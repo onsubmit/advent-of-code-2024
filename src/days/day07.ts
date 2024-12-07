2c48e16500d9db08a5e6d58ba66515078259bd84
Andy <onsubmit@outlook.com>
2024-12-07 12:46:53 -0800

    Add day 7, part 2

--- src/days/day07.ts
+++ src/days/day07.ts
@@ -67,7 +67,49 @@ const getPermutationAt = <T>(index: number, array: Array<T>, length: number): Ar
 };
 
 export const getPartTwoSolution = (input: string): string => {
-  const lines = input.split('\n');
+  const lines: Array<Line> = input.split('\n').map((line) => {
+    const split = line.split(':').map((v) => v.trim());
+    const value = parseInt(split[0], 10);
+    const equation = split[1].split(' ').map((v) => parseInt(v, 10));
+    return {
+      value,
+      equation,
+    };
+  });
 
-  return '';
+  const possiblyTrueEquations = lines.filter((line) => isPossiblyTruePart2(line));
+  const totalCalibrationResult = sumArrayBy(possiblyTrueEquations, (equation) => equation.value);
+  return totalCalibrationResult.toString();
+};
+
+const isPossiblyTruePart2 = (line: Line): boolean => {
+  const permutations = generatePermutationsOfLength(
+    ['+', '*', '||'] as const,
+    line.equation.length - 1
+  );
+  const expected = line.value;
+  for (const operators of permutations) {
+    let actual = line.equation[0];
+    for (let i = 1; i < line.equation.length; i++) {
+      switch (operators[i - 1]) {
+        case '+': {
+          actual += line.equation[i];
+          break;
+        }
+        case '*': {
+          actual *= line.equation[i];
+          break;
+        }
+        case '||': {
+          actual = parseInt(`${actual}${line.equation[i]}`, 10);
+        }
+      }
+    }
+
+    if (actual === expected) {
+      return true;
+    }
+  }
+
+  return false;
 };
