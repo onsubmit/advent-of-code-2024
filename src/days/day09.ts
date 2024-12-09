f8aeb6ec59bdaea9135258cbfbc42ced95a455e1
Andy <onsubmit@outlook.com>
2024-12-09 00:01:31 -0800

    Add day 9, part 2

--- src/days/day09.ts
+++ src/days/day09.ts
@@ -44,6 +44,69 @@ export const getPartOneSolution = (input: string): string => {
   return sum.toString();
 };
 
+type Block = {
+  fileId: number;
+  length: number;
+  moved: boolean;
+};
+
 export const getPartTwoSolution = (input: string): string => {
-  return '';
+  const blocks: Array<Block> = [];
+  [...input].forEach((char, i) => {
+    const repeat = parseInt(char, 10);
+    if (repeat === 0) {
+      return;
+    }
+    if (i % 2 === 0) {
+      blocks.push({ fileId: i / 2, length: repeat, moved: false });
+    } else {
+      blocks.push({ fileId: -1, length: repeat, moved: false });
+    }
+  });
+
+  for (let i = blocks.length - 1; i >= 0; i--) {
+    const right = blocks[i];
+    if (right.fileId < 0 || right.moved) {
+      continue;
+    }
+
+    const index = blocks.findIndex((v) => v.fileId === -1 && v.length >= right.length);
+    if (index >= 0 && index < i) {
+      const diff = blocks[index].length - right.length;
+      if (diff === 0) {
+        right.moved = true;
+        blocks[index].fileId = right.fileId;
+        blocks[i].fileId = -2;
+        i = blocks.length;
+        continue;
+      } else if (diff > 0) {
+        right.moved = true;
+        blocks[index].fileId = right.fileId;
+        blocks[index].length = right.length;
+        blocks[i].fileId = -2;
+        blocks.splice(index + 1, 0, { fileId: -1, length: diff, moved: false });
+        i = blocks.length;
+        continue;
+      }
+    }
+  }
+
+  let position = 0;
+  const sum = sumArrayBy(blocks, (v, i) => {
+    if (v.fileId < 0) {
+      position += v.length - 1;
+      return 0;
+    }
+
+    let s = 0;
+    for (let j = 0; j < v.length; j++) {
+      s += (i + position++) * v.fileId;
+    }
+
+    position--;
+
+    return s;
+  });
+
+  return sum.toString();
 };
